bf2899c33f6fcbb675706d33dbb88d851348fa25
aksesku <serlinurjannah12@gmail.com>
2025-05-05 00:41:43 +0700

    Create particles-config.js

---
+++ js/particles-config.js
@@ -0,0 +1,49 @@
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 80,
+      density: { enable: true, value_area: 800 },
+    },
+    color: { value: "#ffffff" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
+    },
+    opacity: {
+      value: 0.5,
+      anim: { enable: false },
+    },
+    size: {
+      value: 5,
+      random: true,
+      anim: { enable: false },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "none",
+      out_mode: "out",
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "grab" },
+      onclick: { enable: true, mode: "push" },
+      resize: true,
+    },
+    modes: {
+      grab: { distance: 140 },
+      bubble: { distance: 400 },
+      repulse: { distance: 200 },
+    },
+  },
+  retina_detect: true,
+});
